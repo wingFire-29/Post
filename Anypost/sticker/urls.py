1a4893ac1df37bb0ffda683a24f09981cf9104de
ShubhamPandey57 <shubhampandey2869@gmail.com>
2025-08-02 14:36:41 +0530

    templates and other

--- Anypost/sticker/urls.py
+++ Anypost/sticker/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('register/',views.register_view,name='register'),
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logout_view,name='logout'),
+    path('dashboard/',views.dashboard_view,name='dashboard'),
 ]
--- Anypost/sticker/views.py
+++ Anypost/sticker/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+#from django.shortcuts import get_object_or_404,redirect
+#from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout
 
 # Create your views here.
 
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
-def index(request):
-    return render(request,'index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')  
+        else:
+            print("Invalid login credentials") 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login.html')
+
+def dashboard_view(request):
+    return render(request,'dashboard.html')
+
+
+
